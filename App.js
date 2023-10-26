8e4c10f6f98ec5a78a5922dc5edb6e44e4251ebc
felipeCast <castfelipe10@gmail.com>
2023-10-26 17:02:12 -0300

    Corrigindo bug

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/routes/routes';
 
-export default function App() {
 
+export default function App() {
+  LogBox.ignoreAllLogs()
   return (
 
     <NavigationContainer>
